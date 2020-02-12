fb17a2d14ea72101b7226673be5dadc1c64186c8
christian torio <christiantorio@aol.com>
2020-02-12 00:15:41 -0800

    adding some test code using Java

---
+++ testing/DisplayInfo.java
@@ -0,0 +1,7 @@
+public class DisplayInfo{
+    public static void main (String[] args){
+        System.out.println("My name is Chris Torio");   
+        System.out.println("I am 38 years old"); 
+        System.out.println("My hometown is Newark, CA"); 
+    }
+}
