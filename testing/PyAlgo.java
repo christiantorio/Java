28c3424d7865665efc07ad33b2e4d4e152185cbf
christian torio <christiantorio@aol.com>
2020-02-13 21:30:29 -0800

    adding more java test code

---
+++ testing/PyAlgo.java
@@ -0,0 +1,9 @@
+public class PyAlgo {
+    public static void main(String[] args) {
+        // double legA = 12;
+        // double legb = 14;
+        calculateHypotenuse ans = new calculateHypotenuse();
+        double answer = ans.calculateHypotenuse();
+        System.out.println(answer);
+    }
+}
